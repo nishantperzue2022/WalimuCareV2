55c4867b3b2f4f98544e3a2aca1d277b4c9ec419
Peter <Peter@DESKTOP-TA2JGM1>
2023-02-27 16:11:05 +0300

    walimu care app

--- WalimuCareApp/WalimuCareApp/Views/DownloadCenterPage.xaml.cs
+++ WalimuCareApp/WalimuCareApp/Views/DownloadCenterPage.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,12 +11,10 @@ namespace WalimuCareApp.Views
 		{
 			InitializeComponent ();
 		}
-
 		private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
 		{
 
         }
-
 		private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
 		{
 
