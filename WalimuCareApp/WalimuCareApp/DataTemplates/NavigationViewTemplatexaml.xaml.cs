ef10aeda35f3435b463bf470578a7a1d8deb1bd2
Peter <Peter@DESKTOP-TA2JGM1>
2023-03-01 11:57:33 +0300

    data

---
+++ WalimuCareApp/WalimuCareApp/DataTemplates/NavigationViewTemplatexaml.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WalimuCareApp.DataTemplates
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NavigationViewTemplatexaml : ContentView
+    {
+		public NavigationViewTemplatexaml ()
+		{
+			InitializeComponent ();
+		}
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            try
+            {
+                Shell.Current.FlyoutIsPresented = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
--- WalimuCareApp/WalimuCareApp/Utils/MaklAPI.cs
+++ WalimuCareApp/WalimuCareApp/Utils/MaklAPI.cs
@@ -4,7 +4,8 @@ namespace WalimuCareApp.Utils
 {
     public static class MaklAPI
     {
-        public static string ApiUrl = "https://walimu.makl-psms.com/";
+        //public static string ApiUrl = "https://walimu.makl-psms.com/";
+        public static string ApiUrl = "https://collaboration.makl-psms.com/";
         //public static string ApiUrl = "https://ff0e-154-70-3-144.eu.ngrok.io/";
     }
 }
